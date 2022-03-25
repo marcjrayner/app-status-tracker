cc86b35a011460551b854f78c7102ab495b23a11
Marc <marc@whisperclaims.co.uk>
2022-03-25 11:27:46 +0000

    hello world react

---
+++ statuses-web/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import AppStatusContainer from "./components/AppStatusContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <AppStatusContainer />
+    </div>
+  );
+}
+
+export default App;
